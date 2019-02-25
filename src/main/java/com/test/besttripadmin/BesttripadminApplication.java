ee9686a4676ea83f087aa3a1aa55537e50c43051
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2019-02-25 08:58:19 +0100

    Initial commit

---
+++ src/main/java/com/test/besttripadmin/BesttripadminApplication.java
@@ -0,0 +1,18 @@
+package com.test.besttripadmin;
+
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@EnableAdminServer
+public class BesttripadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesttripadminApplication.class, args);
+	}
+
+}
